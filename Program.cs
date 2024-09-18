c34f8ee6d6d4596a88a15325176ca4c8af0af00f
Pfarelo Unarine Nelwamondo <pfarelounarine.nelwamondo@vcontractor.co.za>
2024-09-18 23:10:53 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Reverse Polish Notation string:");
+        string rpnInput = Console.ReadLine();
+
+        try
+        {
+            string infixOutput = ConvertRPNToInfix(rpnInput);
+            Console.WriteLine("Infix Notation: " + infixOutput);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+
+    static string ConvertRPNToInfix(string rpn)
+    {
+        Stack<string> stack = new Stack<string>();
+        string[] tokens = rpn.Split(' ');
+
+        foreach (var token in tokens)
+        {
+            if (IsOperator(token))
+            {
+                string operand2 = stack.Pop();
+                string operand1 = stack.Pop();
+                string expression = $"({operand1} {token} {operand2})";
+                stack.Push(expression);
+            }
+            else
+            {
+                stack.Push(token);
+            }
+        }
+
+        if (stack.Count != 1)
+        {
+            throw new InvalidOperationException("Invalid expression.");
+        }
+
+        return stack.Pop();
+    }
+
+    static bool IsOperator(string token)
+    {
+        return token == "+" || token == "-" || token == "*" || token == "/";
+    }
+}
